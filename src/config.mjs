1057dfb25a016a7e3c7218c2b4140d1e5ae6a22a
Rigo Mejia <rmejia4209@gmail.com>
2025-01-15 15:25:10 -0600

    Modified config.mjs to return atc source URLs

--- src/config.mjs
+++ src/config.mjs
@@ -8,3 +8,8 @@ export const __dirname = import.meta.dirname;
 export const publicPath = path.join(__dirname, 'public');
 export const sessionFolder = path.join(__dirname, 'sessions');
 export const port = process.env.PORT ? process.env.PORT : 3030;
+export const sourceURL = process.env.ATC_FEED_URL;
+export const atcLinks = {
+    KJFK: process.env.KJFK,
+    KORD: process.env.KORD,
+  }
